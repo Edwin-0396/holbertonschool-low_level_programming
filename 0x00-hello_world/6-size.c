84ecf51d1e6e9f249e08bd48f9a0512b6eeac0ae
Edwin-0396 <you@example.com>
2022-02-10 20:45:56 +0000

    new 6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+char a;
+
+
+int main(void)
+{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(float));
+	printf(sizeof(double));
+	printf(sizeof(void));
+	printf(sizeof(long));
+}
