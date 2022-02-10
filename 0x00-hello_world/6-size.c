d021252359e84d15493f118ad7e2a48bd343aeea
Edwin-0396 <you@example.com>
2022-02-10 20:57:18 +0000

    new 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,6 @@
  * Return: Always 0 (Success)
  */
 
-char a;
-
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s) \n",sizeof(char));
@@ -16,4 +13,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s) \n",sizeof(long int));
 	printf("Size of a long long int: %d byte(s) \n",sizeof(long long int));
 	printf("Size of a float: %d byte(s) \n",sizeof(float));
+	return(0);
 }
