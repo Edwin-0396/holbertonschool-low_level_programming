9f2e9b38f55859898a27ac1bcae15c189a100a63
Edwin-0396 <you@example.com>
2022-02-11 23:08:01 +0000

    New 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+* main - program that prints the alphabet in
+* lowercase, and then in uppercase, followed by a new line.
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 0 ; ch < 10 ; ch++)
+		putchar(ch);
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+  return (0);
+}
