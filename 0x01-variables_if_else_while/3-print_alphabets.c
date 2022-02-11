bea7a9f3d2ebc61e099e9e083dd9f11683c47f7b
Edwin-0396 <you@example.com>
2022-02-11 21:08:53 +0000

    new 3-print_alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,8 @@
 
 
 /**
-* main -  program that prints the alphabet in lowercase, and then in uppercase, foll* owed by a new line.
+* main -  program that prints the alphabet in 
+* lowercase, and then in uppercase, followed by a new line.
 *
 * Return: always 0
 */
@@ -12,9 +13,7 @@ int main(void)
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
 		putchar(ch);
-        }
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
                 putchar(ch);
