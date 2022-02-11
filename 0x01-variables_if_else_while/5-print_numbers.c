9ef523887b935a1b037373114122a10b4ceaa3c9
Edwin <37195460+Edwin-0396@users.noreply.github.com>
2022-02-11 16:49:48 -0500

    Change doc

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 
 /**
 * main - program that prints all single digit numbers of base 10
-* starting from 0, fo* llowed by a new line.
+* starting from 0, followed by a new line.
 *
 * Return: always 0
 */
