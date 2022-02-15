ac7783d9af3b89c7475b39ac204e60d6d6641e66
Edwin-0396 <you@example.com>
2022-02-15 15:42:52 +0000

    add new 0-file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,18 @@
 * Return: Always 0.
 */
 
-int print_sign (int n)
+int print_sign(int n)
 {
-	if (n*(-1) < 0)
+	if (n * (-1) < 0)
 	{
 		_putchar('+');
-		return 1;
-	}else if (n*(-1) == 0)
+		return (1);
+	} else if (n * (-1) == 0)
 	{
 		_putchar('0');
-		return 0;
-	}else
-	{
-		_putchar('-');
-		return 1;
+		return (0);
 	}
+
+	_putchar('-');
+	return (1);
 }
