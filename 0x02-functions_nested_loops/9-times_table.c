6901acc2334e61c21860ecb286e516f2b41f2c8f
Edwin-0396 <you@example.com>
2022-02-15 16:40:45 +0000

    add new 0-file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,9 +17,17 @@ void times_table(void)
 		while (m < 10)
 		{
 			times_table = n * m;
-			_putchar((times_table / 10) + '0');
-			_putchar((times_table % 10) + '0');
-			_putchar(',');
+			if (times_table > 10)
+			{
+				_putchar((times_table / 10) + '0');
+				_putchar((times_table % 10) + '0');
+			} else
+				_putchar(times_table + '0')
+			if (m < 9)
+			{			
+				_putchar(',');
+				_putchar(' ');
+			}
 			m++;
 		}
 		_putchar('\n');
