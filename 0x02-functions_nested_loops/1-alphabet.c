c0938cf7b1b268bbcee34ef8c676b1a750f6471e
Edwin-0396 <you@example.com>
2022-02-15 14:51:13 +0000

    add new 0-file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main: function that prints the alphabet, in lowercase,
+* print_alphabet: function that prints the alphabet, in lowercase,
 * followed by a new line.
 *
 * Return: Always 0.
