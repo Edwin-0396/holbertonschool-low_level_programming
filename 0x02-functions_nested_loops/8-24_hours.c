42ec723d604f74605a9753c8dd86764d4004471e
Edwin-0396 <you@example.com>
2022-02-15 16:26:24 +0000

    add new 0-file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,8 +17,8 @@ void jack_bauer(void)
 			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
 			_putchar(':');
-      _putchar(m / 10 + '0');
-      _putchar(m % 10 + '0');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
 			_putchar('\n');
 		}
 	}
