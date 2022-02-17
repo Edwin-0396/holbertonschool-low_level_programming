e4976beceb3f4a4a73b1c5ec3e2f3633351cafca
Edwin <egc4611@gmail.com>
2022-02-17 11:02:24 -0800

    new digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,18 @@
 #include "main.h"
+
 /**
- * _isdigit - check if c is a digit
- * @c: random integrer
- * Return: 1 if c is a digit o if is not
+ *
+ * _isdigit - function that checks for a digit (0 through 9)
+ *
+ * @c: variable to evaluate if is uppercase
+ *
+ * Return: 1 if c is a digit  or 0 is otherwise
+ *
  */
+
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= 0 && c <= 9)
 		return (1);
 	else
 		return (0);
