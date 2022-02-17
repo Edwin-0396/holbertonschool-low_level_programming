6a705421aaa845b99eb08d93d7b39a53e8752eb8
Edwin <egc4611@gmail.com>
2022-02-17 11:17:07 -0800

    new 0-isupper

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *
- * _isdigit - function that checks for a digit (0 through 9)
- *
- * @c: variable to evaluate if is uppercase
- *
- * Return: 1 if c is a digit  or 0 is otherwise
- *
- */
+* _isdigit - function that checks for a digit (0 through 9)
+*
+* @c: variable to evaluate if is uppercase
+*
+* Return: 1 if c is a digit  or 0 is otherwise
+*
+*/
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
