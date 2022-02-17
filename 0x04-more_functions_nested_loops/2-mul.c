78c2d1eff26d807c2e3bb988f0f3ebad9ef6be45
Edwin <37195460+Edwin-0396@users.noreply.github.com>
2022-02-17 12:34:42 -0500

    new multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply a and b
+ * @a: 1 number to multiply
+ * @b: 2 number to multiply
+ * Return: multiply by a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
