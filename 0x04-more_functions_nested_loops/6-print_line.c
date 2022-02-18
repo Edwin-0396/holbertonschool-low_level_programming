cc72ecaa5576a73ac0461821b57476db82301796
Edwin-0396 <you@example.com>
2022-02-18 00:05:55 +0000

    new 6-print

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,24 @@
 #include "main.h"
+
 /**
- * print_line - function that checks for uppercase character.
- * @n: is the integer for the paramaters of my function
- * Return: 0
+ * print_line - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * Return: a straight line
  */
 void print_line(int n)
 {
-int a = 0;
+int c;
 
-if (n > 0)
+if (n <= 0)
 {
-while (a <= n)
-{
-_putchar('_');
-a++;
-}
 _putchar('\n');
 }
 else
+{
+for (c = 1; c <= n; c++)
+{
+_putchar('_');
+}
 _putchar('\n');
+}
 }
