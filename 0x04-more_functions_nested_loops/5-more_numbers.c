f24a3ec7097bdd74faeda2f213d1d9147bf35766
Edwin-0396 <you@example.com>
2022-02-18 17:12:01 +0000

    function that prints 10 times the numbers, from 0 to 14, followed by a new line.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * more_numbers - function that checks for uppercase character
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
  * Return: 0
  */
 void more_numbers(void)
@@ -14,12 +15,18 @@ void more_numbers(void)
 		while (a <= 14)
 		{
 			if (a > 9)
+			{
 				_putchar(a / 10 + '0');
 				_putchar(a % 10 + '0');
-		a++;
+			}
+			else
+			{
+				_putchar(a / 10 + '0');
+			}
+			a++;
 		}
-	_putchar('\n');
-	b++;
-	a = 0;
+		_putchar('\n');
+		b++;
+		a = 0;
 	}
 }
