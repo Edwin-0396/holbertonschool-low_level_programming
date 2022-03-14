fa3e820e5b1a1216240a347627ac19e47a88e5ff
Edwin-9603 <egc4611@gmail.com>
2022-03-14 15:30:27 -0500

    Type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _FILE_
+#define _FILE_
+
+/**
+ * struct dog - structure for dog feats.
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
