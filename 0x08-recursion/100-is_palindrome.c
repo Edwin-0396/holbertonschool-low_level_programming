c3d0da21bc2650c803baf8a704e1d7b5a40aa634
Edwin-9603 <egc4611@gmail.com>
2022-03-18 08:23:30 -0500

    Function that returns the sum of all its parameters.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,10 +10,8 @@
 
 char *to_end(char *s)
 {
-
     if (!*(s+1))
         return (s);
-
     return (to_end(s + 1));
 }
 
@@ -21,29 +19,22 @@ int is_palindrome_out(char *s, char *s1)
 {
     if (*s != *s1)
         return (0);
-
     if (s >= s1)
         return (1);
-
     return (is_palindrome_out(s + 1, s1 - 1));
-
 }
 
 
 int is_palindrome(char *s)
 {
     char *s1;
-
     if (!s)
         return (-1);
-
     if (!*s)
-        return (1); 
-
-    s1 = to_end(s);
-
+        return (1);
+    s1 = to_end(s);+
+    
     return (is_palindrome_out(s, s1));
-
     /**while (*(s1 + 1))
         s1++;
 
---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Function that returns the sum of all its parameters.
+ * @n: number of parameters
+ *
+ * Return: Always 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list ap;
+
+	va_start(ap, n); /* Initialize the argument list. */
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int); /* Get the next argument value. */
+
+	va_end(ap); /* Clean up. */
+	return sum;
+}
