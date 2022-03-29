59d7f02321dbee7b2f4b04e1f7ddfb3db8e3ab0a
Edwin-9603 <egc4611@gmail.com>
2022-03-29 16:18:18 -0500

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,18 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * free_list - frees a list_t list.
+ * @head: head node
  *
  * Return: Always 0.
  */
+
+void free_list(list_t *head)
+{
+	while (head)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
