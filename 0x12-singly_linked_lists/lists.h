9dfd5d598e0319a72378ef69d0d678fa944d2f1e
Edwin-9603 <egc4611@gmail.com>
2022-03-29 14:04:46 -0500

    new readme

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,11 +15,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -27,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif 
+#endif
