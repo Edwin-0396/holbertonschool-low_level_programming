780548a416416a50eb8f1562b43a6898883b123c
Edwin-9603 <egc4611@gmail.com>
2022-03-29 16:04:44 -0500

    add a node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,3 +5,34 @@
  *
  * Return: Always 0.
  */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+	list_t *new, *last;
+	unsigned int len = 0;
+
+	while(str[len])
+		len++;
+
+	new = (list_t *)malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+	last = *head;
+
+	if (!last)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (last->next)
+		last = last->next;
+	last->next = new;
+
+	return (new);
+}
