634d46860cdb318934dc86aac2e30bac9a61c67d
Edwin-9603 <egc4611@gmail.com>
2022-04-01 16:15:43 -0500

    insert_node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,37 +1,39 @@
 #include "lists.h"
 
 /**
- * main - check the code
- * 
+ * insert_nodeint_at_index - function that inserts a
+ * new node at a given position.
+ *@head: head node
+ * @n: element of the node
+ * @idx: index to add the node
  * Return: Always 0.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
-	listint_t *new,*aux = *head;
+	listint_t *new, *aux = *head;
 	unsigned int count = 0;
 
-	if(!*head)
-		return(NULL);
+	if (!*head)
+		return (NULL);
 
-	new = malloc (sizeof(listint_t));
-	if(!new)
-		return(NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-	while(*head)
+	while (*head)
 	{
-		if(count == idx - 1)
-		{	
+		if (count == idx - 1)
+		{
 			new->n = n;
 			new->next = aux->next;
 			aux->next = new;
 			return (new);
 		}
-		if(aux->next)
+		if (aux->next)
 			aux = aux->next;
 		count++;
 	}
 	return (NULL);
-
 }
