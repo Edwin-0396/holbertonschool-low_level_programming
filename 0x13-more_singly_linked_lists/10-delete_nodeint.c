c64cc64064b5c08e8c9f2a8e79e4cd3242c0c5f9
Edwin-9603 <egc4611@gmail.com>
2022-04-01 16:35:39 -0500

    delete_node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,37 @@
 #include "lists.h"
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
+ * delete_nodeint_at_index - remove a node at a given position
+ * @head: linked list
+ * @index: position of the node to remove
+ * Return: Always 0 (Success)
  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head, *current = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,40 +1,40 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts a
- * new node at a given position.
- *@head: head node
- * @n: element of the node
- * @idx: index to add the node
- * Return: Always 0.
+ * insert_nodeint_at_index - function that inserts a new
+ * node at a given position.
+ * @head: head of the list
+ * @index: location to insert node
+ * @n: value of the inserted node
+ * Return: pointer to head of list
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-	listint_t *current, *new;
+	listint_t *aux, *new;
 
-	current = *head;
+	aux = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	if (index == 0)
 	{
-		new->next = current;
+		new->next = aux;
 		*head = new;
 		return (*head);
 	}
 	while (index > 1)
 	{
-		current = current->next;
+		aux = aux->next;
 		index--;
-		if (!current)
+		if (!aux)
 		{
 			free(new);
 			return (NULL);
 		}
 	}
-	new->next = current->next;
-	current->next = new;
+	new->next = aux->next;
+	aux->next = new;
 	return (new);
 }
