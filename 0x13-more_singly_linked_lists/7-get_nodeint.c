c6e6201acb25390022e70e51e260202087b949ee
Edwin-9603 <egc4611@gmail.com>
2022-04-01 14:53:03 -0500

    get an index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,10 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	if(!head)
-		return(NULL);
+	if (!head)
+		return (NULL);
 
-	if (index > 0) 
-		return(get_nodeint_at_index(head->next, index - 1));
-	return(head);	
+	if (index > 0)
+		return (get_nodeint_at_index(head->next, index - 1));
+	return (head);
 }
