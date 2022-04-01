5351a9f0616ea45b9baac72061330eee579e338f
Edwin-9603 <egc4611@gmail.com>
2022-04-01 11:38:39 -0500

    free-list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,18 +8,11 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *aux;
-
 	if (!head)
 		return;
 
-	aux = *head;
-	while (*head)
-	{
-		aux = *head;
-		*head = (*head)->next;
-		free(aux);
-	}
+	if (*head)
+		free_listint2(&((*head)->next));
 	free(*head);
 	*head = NULL;
 }
