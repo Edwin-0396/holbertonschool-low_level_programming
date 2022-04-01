3bd01d9cdbc733d938031b6178724e0491e402b9
Edwin-9603 <egc4611@gmail.com>
2022-04-01 16:11:41 -0500

    insert_node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,3 +5,33 @@
  * 
  * Return: Always 0.
  */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+	listint_t *new,*aux = *head;
+	unsigned int count = 0;
+
+	if(!*head)
+		return(NULL);
+
+	new = malloc (sizeof(listint_t));
+	if(!new)
+		return(NULL);
+
+	while(*head)
+	{
+		if(count == idx - 1)
+		{	
+			new->n = n;
+			new->next = aux->next;
+			aux->next = new;
+			return (new);
+		}
+		if(aux->next)
+			aux = aux->next;
+		count++;
+	}
+	return (NULL);
+
+}
