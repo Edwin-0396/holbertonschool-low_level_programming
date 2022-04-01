efe5954cc42d529306290c9a5b46d8b575be9c8c
Edwin-9603 <egc4611@gmail.com>
2022-04-01 14:35:40 -0500

    pop_listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,22 @@
 #include "lists.h"
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
+ * pop_listint - check the code
+ *@head: head node
+ * Return: element deleted.
  */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *aux;
+
+	if (!head || !*head)
+		return (0);
+
+	n = (*head)->n;
+	aux = *head;
+	*head = (*head)->next;
+	free(aux);
+	return (n);
+}
