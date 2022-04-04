2b3ecae37fb93d8be6ce112d505d980759b6005b
Edwin-9603 <egc4611@gmail.com>
2022-04-04 16:52:29 -0500

    function that returns the number of bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: number to set the value of
+ * @index: the bit you want to set
  *
- * Return: Always 0.
+ * Return: 1 if it worked, -1 if it didn't
  */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * flip_bits - function that returns the number of bits you would need to flip to
+ * get from one number to another.
  *
  * Return: Always 0.
  */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i = 0;
+	unsigned long int xor = n ^ m;
+
+	while (xor)
+	{
+		if (xor & 1)
+			i++;
+		xor >>= 1;
+	}
+	return (i);
+}
