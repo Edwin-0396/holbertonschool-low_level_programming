7610be622a2256b494af7745c33aa05fd1fea5b1
Edwin-9603 <egc4611@gmail.com>
2022-04-04 16:56:28 -0500

    function that returns the number of bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * flip_bits - function that returns the number of bits you would need to flip to
+ * flip_bits - function that returns the number
+ * of bits you would need to flip to
  * get from one number to another.
+ * @n: first number
+ * @m: second number
  *
  * Return: Always 0.
  */
