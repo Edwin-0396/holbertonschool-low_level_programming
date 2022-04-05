efbd90934ecbfc4652021498ae0d143795b35d1c
Edwin-9603 <egc4611@gmail.com>
2022-04-05 16:51:33 -0500

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT);
+		fd = open(filename, O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
-		close(fd);
 		return (1);
 	}
 
