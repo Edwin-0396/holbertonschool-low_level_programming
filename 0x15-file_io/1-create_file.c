e9ab9522feade453f0b9a1d8f1852e0e5b8410e6
Edwin-9603 <egc4611@gmail.com>
2022-04-05 15:51:38 -0500

    0x15. C - File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * create_file - function that creates a file.
+ *@filename: the name of the file to create
+ *@text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	
+
+
+
+
+}
