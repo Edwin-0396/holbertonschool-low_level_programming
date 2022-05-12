d0b37f3b564f71e3d875c24766dd2299ec4ec8e5
Edwin-9603 <egc4611@gmail.com>
2022-05-12 17:20:24 -0500

    2-addnode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,35 +2,24 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: pointer to a pointer to a struct of type dlistint_t
- * @n: n value of new node
- * Return: address of new element
+ * @head: double pointer to head node
+ * @n: variable in a struct dlistint_t
+ * Return: new node
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = NULL;
-
-	if (!head)
-		return (NULL);
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-
-	new->prev = NULL;
 	new->n = n;
-	new->next = NULL;
-
-	if (!*head)
-	{
-		new->next = *head;
-		*head = new;
-		free(new);
-		return (new);
-	}
+	new->prev = NULL;
 	new->next = *head;
-	(*head)->prev = new;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
+
 	return (new);
 }
