c94a6321115727701ec55939dd56cbdff341bc36
Edwin-9603 <egc4611@gmail.com>
2022-05-12 18:15:41 -0500

    0-dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,15 @@
  * Return: number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *head)
 {
 	int counter = 0;
 
-	while (h)
+	while (head)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", head->n);
 		counter++;
-		h = h->next;
+		head = head->next;
 	}
 	return (counter);
 }
